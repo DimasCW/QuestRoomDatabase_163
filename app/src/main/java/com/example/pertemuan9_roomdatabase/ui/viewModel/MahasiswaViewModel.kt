408aa6a3254bdc689f57513ce5076e0dbaaade99
DimasCW <cdimas292@gmail.com>
2024-12-15 09:21:56 +0700

    menambahkan data class MhsUIState dan FormErrorState

---
+++ app/src/main/java/com/example/pertemuan9_roomdatabase/ui/viewModel/MahasiswaViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.pertemuan9_roomdatabase.ui.viewModel
+
+import com.example.pertemuan9_roomdatabase.data.entity.Mahasiswa
+
+data class MhsUIState(
+    val MahasiswaEvent : MahasiswaEvent = MahasiswaEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+    val snackBarMessage: String? = null,
+)
+
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null,
+){
+    fun isValid(): Boolean{
+        return nim == null && jenisKelamin == null &&
+                alamat == null && kelas == null && angkatan == null
+    }
+}
+
