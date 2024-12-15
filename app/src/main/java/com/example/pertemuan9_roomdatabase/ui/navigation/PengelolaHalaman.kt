1e9ef5a772ecdd2b13656fc175bf73b5b1cb59d1
DimasCW <cdimas292@gmail.com>
2024-12-15 09:51:42 +0700

    Memperbaiki import pada PengelolaHalaman.kt

--- app/src/main/java/com/example/pertemuan9_roomdatabase/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pertemuan9_roomdatabase/ui/navigation/PengelolaHalaman.kt
@@ -2,21 +2,27 @@ package com.example.pertemuan9_roomdatabase.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringArrayResource
 import androidx.navigation.NavController
 import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan9_roomdatabase.ui.view.mahasiswa.DestinasiInsert
+import com.example.pertemuan9_roomdatabase.ui.view.mahasiswa.InsertMhsView
 
 @Composable
 fun PengelolaHalaman(
-    navController: NavController = rememberNavController(),
+    navController: NavHostController = rememberNavController(),
     modifier: Modifier = Modifier
-) {
+){
     NavHost(
         navController = navController, startDestination = DestinasiInsert.route
-    ){
+    ) {
         composable(
             route = DestinasiInsert.route
-        ){
+        ) {
             InsertMhsView(
                 onBack = {}, onNavigate = { }
             )
