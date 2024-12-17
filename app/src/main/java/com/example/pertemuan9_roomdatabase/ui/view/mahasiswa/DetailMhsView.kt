54842ed9279daa6399f6c4710b5a4dabf0fa9a77
DimasCW <cdimas292@gmail.com>
2024-12-17 10:50:34 +0700

    file DetailMhsView.kt sudah lengkap

--- app/src/main/java/com/example/pertemuan9_roomdatabase/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/pertemuan9_roomdatabase/ui/view/mahasiswa/DetailMhsView.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -19,6 +20,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -28,8 +30,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.view.KeyEventDispatcher.Component
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan9_roomdatabase.data.entity.Mahasiswa
@@ -179,4 +184,54 @@ fun ItemDetailMhs(
         }
     }
 
+}
+
+@Composable
+fun ComponentDetailMhs (
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String
+){
+    Column (
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ){
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya, fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit, onDeleteClick: () -> Unit, modifier: Modifier = Modifier
+){
+    AlertDialog(
+        onDismissRequest ={/* Do Nothing*/},
+        title = { Text("Delete Data") },
+        text = { Text("Apakah anada yakin ingin menghapus data?") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(
+                onClick = onDeleteConfirm
+            ) {
+                Text(
+                    text = "Yes"
+                )
+            }
+        }
+
+    )
 }
