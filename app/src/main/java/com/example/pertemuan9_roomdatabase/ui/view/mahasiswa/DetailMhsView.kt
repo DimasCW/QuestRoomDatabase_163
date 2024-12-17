7d32fd1661ae5042e339b19993cdd7668c7c1546
DimasCW <cdimas292@gmail.com>
2024-12-17 10:27:57 +0700

    menambahkan function BodyDetailMhs

--- app/src/main/java/com/example/pertemuan9_roomdatabase/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/pertemuan9_roomdatabase/ui/view/mahasiswa/DetailMhsView.kt
@@ -1,22 +1,39 @@
 package com.example.pertemuan9_roomdatabase.ui.view.mahasiswa
 
+import android.graphics.Paint.Align
 import android.media.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan9_roomdatabase.ui.viewModel.DetailMhsViewModel
+import com.example.pertemuan9_roomdatabase.ui.viewModel.DetailUiState
 import com.example.pertemuan9_roomdatabase.ui.viewModel.HomeMhsViewModel
 import com.example.pertemuan9_roomdatabase.ui.viewModel.PenyediaViewModel
+import com.example.pertemuan9_roomdatabase.ui.viewModel.toMahasiswaEntity
 
 @Composable
 fun DetailMhsViewModel(
@@ -63,4 +80,67 @@ fun DetailMhsViewModel(
     }
 }
 
+@Composable
+fun BodyDetailMhs(
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState = DetailUiState(),
+    onDeleteClick: () -> Unit = {}
+){
+    var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+    when{
+        detailUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Center
+            ){
+                CircularProgressIndicator()//tampilkan loading
+            }
+        }
 
+        detailUiState.isUiEventnotEmpty -> {
+            Column (
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ){
+                ItemDetailMhs(
+                    mahasiswa = detailUiState.detailUiEvent.toMahasiswaEntity(),
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                Button(
+                    onClick = {
+                        deleteConfirmationRequired = true
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Delete")
+                }
+                if (deleteConfirmationRequired){
+                    DeleteConfirmationDialog(
+                        onDeleteConfirm = {
+                            deleteConfirmationRequired = false
+                            onDeleteClick()
+                        },
+                        onDeleteCancel = {deleteConfirmationRequired = false},
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+
+        detailUiState.isUiEventEmpty -> {
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Data tidak ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+
+    }
+
+}
