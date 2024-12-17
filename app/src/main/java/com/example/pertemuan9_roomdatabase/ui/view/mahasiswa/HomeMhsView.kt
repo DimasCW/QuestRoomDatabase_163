c0932901e9c2802a4f583589cd1ccfc10ba52749
DimasCW <cdimas292@gmail.com>
2024-12-17 10:05:11 +0700

    menambahkan function CardMhs

--- app/src/main/java/com/example/pertemuan9_roomdatabase/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/pertemuan9_roomdatabase/ui/view/mahasiswa/HomeMhsView.kt
@@ -1,16 +1,29 @@
 package com.example.pertemuan9_roomdatabase.ui.view.mahasiswa
 
+import android.graphics.Paint.Align
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -20,16 +33,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan9_roomdatabase.data.entity.Mahasiswa
 import com.example.pertemuan9_roomdatabase.repository.RepositoryMhs
 import com.example.pertemuan9_roomdatabase.ui.viewModel.HomeMhsViewModel
 import com.example.pertemuan9_roomdatabase.ui.viewModel.HomeUiState
 import com.example.pertemuan9_roomdatabase.ui.viewModel.PenyediaViewModel
-import kotlinx.coroutines.NonCancellable.message
-
+import kotlinx.coroutines.internal.ExceptionSuccessfullyProcessed.message
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HoeMhsView(
     viewModel: HomeMhsViewModel = viewModel(factory = PenyediaViewModel.Factory),
@@ -94,13 +110,116 @@ fun BodyHomeMhsView(
         homeUiState.isError -> {
             //Menampilkan pesan error
             LaunchedEffect (homeUiState.errorMessage){
-                homeUiState.errorMessage ?. let(message ->
+                homeUiState.errorMessage?.let(message ->
                 coroutineScope.launch {
                     snackbarHostState.showSnackbar(message)
                 }
             }
         }
+
+        homeUiState.listMhs.isEmpty() -> {
+            //Menampilkan pesan jika data kosong
+            Box(modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Tidak ada data mahasiswa.",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+        else -> {
+            //Menampilkan daftar mahasiswa
+            ListMahasiswa(
+                listMhs = HomeUiState.listMhs,
+                onClick = {
+                    onClick(it)
+                    println(
+                        it
+                    )
+                },
+                modifier = modifier
+            )
+        }
+    }
+
 }
 
+@Composable
+fun ListMahasiswa(
+    listMhs : List<Mahasiswa>,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = {}
+){
+    LazyColumn (
+        modifier = modifier
+    ){
+        items(
+            items = listMhs,
+            itemContent = { mhs ->
+                CardMhs(
+                    mhs = mhs,
+                    onClick = { onClick(mhs.nim)}
+                )
+            }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardMhs(
+    mhs: Mahasiswa,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = {}
+){
+    Card (
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ){
+        Column (
+            modifier = Modifier.padding(8.dp)
+        ){
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.Person, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = mhs.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.DateRange, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = mhs.nim,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
+                )
+            }
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.Home, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = mhs.kelas,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    }
 
 }
